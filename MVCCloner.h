8686a2ac8b8c6c8c64263a8b6ce734daecb5ce17
zx1239856 <zx1239856@gmail.com>
2019-03-08 21:36:47 +0800

    first commit

---
+++ MVCCloner.h
@@ -0,0 +1,43 @@
+/*
+ * Created by zx on 19-3-8.
+ */
+
+#ifndef MVC_MVCCLONER_H
+#define MVC_MVCCLONER_H
+
+#include <opencv2/core/core.hpp>
+#include "AdaptiveMesh.h"
+
+class MVCCloner {
+public:
+    MVCCloner(mesh::AdaptiveMesh &mesh, cv::Mat &src, cv::Mat &dst);
+
+    void startClone(int delta_x, int delta_y);
+
+    cv::Mat getResult();
+
+private:
+    mesh::AdaptiveMesh &_mesh;
+    cv::Mat &_src;
+    cv::Mat &_dst;
+    cv::Mat _output;
+
+    // helper methods
+    static int round(double val) { return val + .5; }
+    static uchar roundAndNormalize(double val) 
+    { 
+        if(val < 0)return 0;
+        else if(val > 255)return 255;
+        else return static_cast<uchar>(round(val));
+    }
+    static double getTan(const mesh::Point &origin, const mesh::Point &a, const mesh::Point &b);
+    static double getDist(const mesh::Point &a, const mesh::Point &b);
+    static uchar getPixel(const cv::Mat &img, int x, int y, int ch);
+    static void setPixel(cv::Mat &img, uchar val, int x, int y, int ch);
+    static std::tuple<double, double, double> getInterpolationParams(const mesh::Point &p1, double c1, const mesh::Point &p2, double c2, const mesh::Point & p3, double c3);
+    static double crossProduct(const mesh::Point &p1, const mesh::Point &p2);
+    static bool inTriangle(const mesh::Point &p1, const mesh::Point &p2, const mesh::Point &p3, const mesh::Point &p);
+};
+
+
+#endif //MVC_MVCCLONER_H
---
+++ main.cpp
@@ -0,0 +1,154 @@
+#include <opencv2/highgui.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <glog/logging.h>
+#include <gflags/gflags.h>
+#include "AdaptiveMesh.h"
+#include "MVCCloner.h"
+
+DEFINE_bool(interactive, false, "Interactive mode switch(true for GUI, false for CLI)");
+DEFINE_string(src, "", "Source img file location");
+DEFINE_string(dst, "", "Target img file location");
+DEFINE_string(mask, "", "Mask file location");
+DEFINE_string(out, "", "Output img file location");
+DEFINE_int32(x, 0, "Position x to put source img");
+DEFINE_int32(y, 0, "Position y to put source img");
+
+namespace mask {
+    constexpr char mask_window_name[] = "Mask Selection(Enter to confirm, c to clear)";
+    constexpr char target_pos_sel_window[] = "Target position selection";
+
+    cv::Point prev_point(-1, -1);
+    cv::Point sel_point(-1, -1);
+    cv::Mat mask;
+    cv::Mat orig_img;
+    cv::Mat orig_dst_img;
+    cv::Mat dst_img;
+
+    void onMouse(int evt, int x, int y, int flags, void *param) {
+        if (evt == CV_EVENT_LBUTTONUP || !(flags & CV_EVENT_FLAG_LBUTTON))
+            prev_point = cv::Point(-1, -1);
+        else if (evt == CV_EVENT_LBUTTONDOWN)
+            prev_point = cv::Point(x, y);
+        else if (evt == CV_EVENT_MOUSEMOVE && (flags & CV_EVENT_FLAG_LBUTTON)) {
+            cv::Point curr(x, y);
+            if (prev_point.x > 0) {
+                cv::line(mask, prev_point, curr, cv::Scalar::all(255));
+                cv::line(orig_img, prev_point, curr, cv::Scalar::all(255));
+            }
+            prev_point = curr;
+            cv::imshow(mask_window_name, orig_img);
+        }
+    }
+
+    void onMouseSelDest(int evt, int x, int y, int flags, void *param) {
+        if (evt == CV_EVENT_LBUTTONDOWN) {
+            dst_img = orig_dst_img.clone();
+            sel_point = cv::Point(x, y);
+            cv::drawMarker(dst_img, sel_point, cv::Scalar(0, 20, 255));
+            cv::rectangle(dst_img, sel_point, cv::Point(x + orig_img.cols, y + orig_img.rows), cv::Scalar(0, 100, 255));
+            cv::imshow(target_pos_sel_window, dst_img);
+        }
+    }
+}
+
+int main(int argc, char **argv) {
+    gflags::SetUsageMessage(
+            "MVC Implementation by zx1239856. \nNote: Some CLI options not available when Interactive mode is on");
+    google::ParseCommandLineFlags(&argc, &argv, true);
+    google::InitGoogleLogging(argv[0]);
+    google::LogToStderr();
+
+    std::vector<mesh::Point> boundary;
+    if (FLAGS_interactive)
+        CHECK(FLAGS_src != "" && FLAGS_dst != "") << "Source and target should be specified";
+    else
+        CHECK(FLAGS_src != "" && FLAGS_dst != "" && FLAGS_mask != "") << "Source, target and mask should be specified";
+    cv::Mat src_img = cv::imread(FLAGS_src);
+    cv::Mat dst_img = cv::imread(FLAGS_dst);
+    CHECK(src_img.data != nullptr) << "Source img load failed";
+    CHECK(dst_img.data != nullptr) << "Target img load failed";
+    if (FLAGS_x > dst_img.cols) {
+        LOG(WARNING) << "Destination x invalid, set to default";
+        FLAGS_x = 0;
+    }
+    if (FLAGS_y > dst_img.rows) {
+        LOG(WARNING) << "Destination y invalid, set to default";
+        FLAGS_y = 0;
+    }
+
+    mask::orig_img = src_img.clone();
+
+    if (!FLAGS_mask.empty()) {
+        cv::Mat mask_img;
+        mask_img = cv::imread(FLAGS_mask, 0);
+        CHECK(mask_img.data != nullptr) << "Mask load failed";
+        std::vector<std::vector<cv::Point> > contours;
+        cv::findContours(mask_img, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+        CHECK(contours.size() > 0) << "Error calculating contours";
+        for (auto &it: contours[0]) {
+            boundary.emplace_back(mesh::Point(it.x, it.y));
+        }
+    } else {
+        cv::Mat empty_mask = cv::Mat(src_img.size(), CV_8U, cv::Scalar::all(0));
+        mask::mask = empty_mask.clone();
+        cv::namedWindow(mask::mask_window_name);
+        cv::setMouseCallback(mask::mask_window_name, &mask::onMouse, 0);
+        while (true) {
+            cv::imshow(mask::mask_window_name, mask::orig_img);
+            int code = cv::waitKey(0);
+            if (code == 10 || code == 13) // `enter` key
+            {
+                LOG(INFO) << "Begin to process mask";
+                std::vector<std::vector<cv::Point> > contours;
+                cv::findContours(mask::mask, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+                mask::mask = empty_mask;
+                cv::drawContours(mask::mask, contours, -1, cv::Scalar(255), CV_FILLED);
+                cv::erode(mask::mask, mask::mask, cv::Mat()); // erode extra lines
+                contours.clear();
+                cv::findContours(mask::mask, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+                CHECK(contours.size() > 0) << "Error calculating contours";
+                for (auto &it: contours[0]) {
+                    boundary.emplace_back(mesh::Point(it.x, it.y));
+                }
+                break;
+            } else if (code == 'c') // clear selection
+            {
+                mask::orig_img = src_img.clone();
+                mask::mask = empty_mask.clone();
+            }
+        }
+        cv::destroyWindow(mask::mask_window_name);
+    }
+    if (FLAGS_interactive) {
+        mask::orig_dst_img = dst_img;
+        mask::dst_img = dst_img.clone();
+        cv::namedWindow(mask::target_pos_sel_window);
+        cv::setMouseCallback(mask::target_pos_sel_window, &mask::onMouseSelDest, 0);
+        while (true) {
+            cv::imshow(mask::target_pos_sel_window, dst_img);
+            int code = cv::waitKey(0);
+            if (code == 10 || code == 13) // `enter` key
+                break;
+        }
+        FLAGS_x = mask::sel_point.x;
+        FLAGS_y = mask::sel_point.y;
+        cv::destroyWindow(mask::target_pos_sel_window);
+
+    }
+    mesh::AdaptiveMesh adp_mesh(boundary);
+    adp_mesh.calcPoints();
+    cv::imshow("Mesh", adp_mesh.visualize(src_img.size()));
+    cv::waitKey(10);
+    MVCCloner cloner(adp_mesh, src_img, dst_img);
+    cloner.startClone(FLAGS_x, FLAGS_y);
+    auto result = cloner.getResult();
+    if (!FLAGS_out.empty()) {
+        cv::imwrite(FLAGS_out, result);
+        LOG(INFO) << "Writing to file " << FLAGS_out;
+    }
+    cv::imshow("Final result", result);
+    cv::waitKey();
+    cv::destroyAllWindows();
+    return 0;
+}
